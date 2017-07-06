9c27d8ab4a70cf0aaf3b8085c78f01135978ffa7
Adam Pugh <adampugh@zoho.com>
2017-07-06 22:13:05 +0100

    Updated mlabs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://<test>:<test>@ds151222.mlab.com:51222/node-todo-api-db");
+mongoose.connect("mongodb://test:test@ds151222.mlab.com:51222/node-todo-api-db");
 
 
 module.exports = {mongoose};
