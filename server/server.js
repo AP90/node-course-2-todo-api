50aecf20680c5859ee4565babbca14ff16de4955
Adam Pugh <adampugh@zoho.com>
2017-07-06 14:47:22 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,7 +22,13 @@ app.post("/todos", (req, res) => {
     })
 });
 
-
+app.get("/todos", (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
 
 
@@ -30,7 +36,7 @@ app.post("/todos", (req, res) => {
 
 
 app.listen(3000, () => {
-    console.log("Started on port 300")
+    console.log("Started on port 3000")
 });
 
 
