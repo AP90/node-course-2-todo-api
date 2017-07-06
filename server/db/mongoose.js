aa9b9da0e40d39f0da982d9c5e010cce26992def
Adam Pugh <adampugh@zoho.com>
2017-07-06 22:00:50 +0100

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect("mongodb://test:test@@ds151222.mlab.com:51222/node-todo-api-db");
 
 module.exports = {mongoose};
-
-
-
