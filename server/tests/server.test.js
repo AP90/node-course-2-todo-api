1df0085fb30db5f9bee38988acbb6cce207c69ef
Adam Pugh <adampugh@zoho.com>
2017-07-07 01:06:21 +0100

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [
         text: "Review Duolingo"
     },
     {   _id: new ObjectID(),
-        text: "Start new Duolingo"
+        text: "Start new Duolingo",
+        completed: true,
+        completedAt: 333
     },
     {   _id: new ObjectID(),
         text: "Finish Node.js course"
@@ -148,4 +150,45 @@ describe("DELETE /todos/:id", () => {
         .expect(404)
         .end(done);
     });
-})
+});
+
+describe("PATCH /todos/:id", () => {
+    it("should update the todo", (done) => {
+        var id = todos[0]._id.toHexString();
+        var text = "This should be the new text";
+
+        request(app)
+        .patch(`/todos/${id}`)
+        .send({
+            completed: true,
+            text
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completed).toBe(true);
+            expect(res.body.todo.completedAt).toBeA("number");
+        })
+        .end(done);
+    });
+
+    it("should clear completedAt when todo is not compelted", (done) => {
+        var id = todos[1]._id.toHexString();
+        var text = "Ths is ta ne text";
+
+        request(app)
+        .patch(`/todos/${id}`)
+        .send({
+            completed: false,
+            text
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completed).toBe(false);
+            expect(res.body.todo.completedAt).toNotExist();
+        })
+        .end(done);
+    });
+
+});
