7887131ea0ef93aa2fc131e74c264f934a741599
Adam Pugh <adampugh@zoho.com>
2017-07-07 22:53:54 +0100

    Added user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 // mongo will create a db called users
 var User = mongoose.model("User", {
@@ -6,8 +7,29 @@ var User = mongoose.model("User", {
         type: String,
         required: true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        unique: true,
+        validate: {
+            // comes from validator package
+            validator: validator.isEmail,
+            message: `{VALUE} is not a valid email`
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 
